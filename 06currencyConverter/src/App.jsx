1101e4451c6aeec73514be3b9ae5f88b704a4862
Samir khan <itssameer75@gmail.com>
2023-10-29 16:06:03 +0530

    desined a custom hook, called useCurrencyInfo

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1 className='text-3xl bg-slate-500'>lets do it</h1>
+    </>
+  )
+}
+
+export default App

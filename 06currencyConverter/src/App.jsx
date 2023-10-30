4fcb01a721a5563391c971ca419e1a679ae8fa1a
Samir khan <itssameer75@gmail.com>
2023-10-30 21:38:07 +0530

    Started here: working on app.jsx

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,7 +1,18 @@
 import { useState } from 'react'
+import {inputBox} from './components'
+import useCurrencyInfo from './Hooks'
+
 
 function App() {
- return(
+
+  const [amount, setAmount] = useState(0)
+  const [from , setFrom] = useState("Usd")
+  const [to, setTo] = useState("Inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  
+
+
+  return(
    <></>
  ) 
 
